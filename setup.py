1de6d0e7ff5f7bd73a4810ca06edf531061389cc
Martin Manns <mmanns@gmx.net>
2021-11-05 19:25:52 +0100

    Python version 3.10 removed from setup.py because it is untested.

--- setup.py
+++ setup.py
@@ -83,7 +83,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Topic :: Office/Business :: Financial :: Spreadsheet',
         'Topic :: Scientific/Engineering',
     ],
