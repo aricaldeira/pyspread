24f613e13816c9d4f70269b0f621d1718951950c
Martin Manns <mmanns@gmx.net>
2023-06-26 21:08:22 +0200

    Gitlab CI fix, pyqt6 version lowered to enable Debian sid

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     keywords=['spreadsheet', 'pyspread'],
     python_requires='>=3.6',
     requires=['numpy (>=1.1)',
-              'PyQt6 (>=6.5)',
+              'PyQt6 (>=6.4)',
               'setuptools (>=40.0)',
               'markdown2 (>=2.3)'],
     extras_require={
