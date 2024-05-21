09fd83b738448177cfc8c58d711fc378605aab0f
Martin Manns <mmanns@gmx.net>
2024-05-21 21:34:22 +0200

    Issue #122: Added tooltip functionality to docs

--- apidocs/build_docs.sh
+++ apidocs/build_docs.sh
@@ -3,11 +3,9 @@
 echo $BASH_SOURCE
 
 HERE_DIR=`dirname "$BASH_SOURCE"`
-ROOT_DIR=`realpath $HERE_DIR/../..`
+ROOT_DIR=`realpath $HERE_DIR/..`
 
 echo $ROOT_DIR
-
-cd $ROOT_DIR/apidocs
 
 
 sphinx-build -a -b html $ROOT_DIR/apidocs $ROOT_DIR/public/
