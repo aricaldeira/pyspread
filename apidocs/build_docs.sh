ec5b645973f5cc060dcfd41f6d1e4885208f79fb
Martin Manns <mmanns@gmx.net>
2024-05-21 21:17:27 +0200

    CI fix

--- apidocs/build_docs.sh
+++ apidocs/build_docs.sh
@@ -3,8 +3,11 @@
 echo $BASH_SOURCE
 
 HERE_DIR=`dirname "$BASH_SOURCE"`
-ROOT_DIR=`realpath $HERE_DIR/..`
+ROOT_DIR=`realpath $HERE_DIR/../..`
 
 echo $ROOT_DIR
+
+cd $ROOT_DIR/apidoc
+
 
 sphinx-build -a -b html $ROOT_DIR/apidocs $ROOT_DIR/public/
